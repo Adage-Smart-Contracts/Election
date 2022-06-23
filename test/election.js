96bdbccfe3621bcd4a984aa03728a6221d09f499
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-06-24 04:34:55 +0530

    Testing candidates

---
+++ test/election.js
@@ -0,0 +1,15 @@
+// Testing the Candidates!
+var Election = artifacts.require("./Election.sol");
+
+contract("Election", function(accounts) {
+  var electionInstance;
+
+  it("initializes with two candidates", function() {
+    return Election.deployed().then(function(instance) {
+      return instance.candidatesCount();
+    }).then(function(count) {
+      assert.equal(count, 2);
+    });
+  });
+
+})
