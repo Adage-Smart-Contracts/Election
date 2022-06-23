d31a6b7146308d0e9b56e5a40e8e1e13e29be3cc
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-06-23 15:14:51 +0530

    Added Model for Candidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,9 +4,29 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Election {
     string public candidate;
 
-    // Constructor
-   constructor () public {
-        candidate = "Candidate1";
+    // Model a Candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
 
+    //Store Candidate
+
+    //Fetch a candidate
+    mapping(uint256 => Candidate) public candidates;
+
+    //Store Candidates Count
+    uint256 public candidatesCount;
+
+    // Constructor
+    constructor() public {
+        addCandidates("Candidate-1");
+        addCandidates("Candidate-2");
+    }
+
+    function addCandidates(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
