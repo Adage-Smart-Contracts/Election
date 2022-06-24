1802ccdb50835b7003fd2b5ec43828d91fa2d8ca
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-06-24 08:20:33 +0530

    Election added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,32 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Election {
-    string public candidate;
 
+contract Election {
     // Model a Candidate
     struct Candidate {
-        uint256 id;
+        uint id;
         string name;
-        uint256 voteCount;
+        uint voteCount;
     }
 
-    //Store Candidate
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
 
-    //Fetch a candidate
-    mapping(uint256 => Candidate) public candidates;
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
-    //Store Candidates Count
-    uint256 public candidatesCount;
-
-    // Constructor
-    constructor() public {
-        addCandidates("Candidate-1");
-        addCandidates("Candidate-2");
+    constructor () public {
+        addCandidate("Vithushan");
+        addCandidate("Shadurshi");
     }
 
-    function addCandidates(string memory _name) private {
-        candidatesCount++;
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+       emit votedEvent(_candidateId);
     }
 }
